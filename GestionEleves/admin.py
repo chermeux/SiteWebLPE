8e445c039f0163989e4ff73da26182947b5644b4
chermeux <76485835+chermeux@users.noreply.github.com>
2023-07-11 13:56:34 +0200

    Delete GestionEleves directory

--- GestionEleves/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from GestionEleves.models import Groupe, Eleve, Appelle, Status
-
-
-# Register your models here.
-admin.site.register(Groupe)
-admin.site.register(Eleve)
-admin.site.register(Appelle)
-admin.site.register(Status)
