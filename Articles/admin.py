32f7cfbe5acd368578df980345d2d9c88a3c5248
chermeux <76485835+chermeux@users.noreply.github.com>
2023-07-11 13:56:22 +0200

    Delete Articles directory

--- Articles/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from Articles.models import Classe, Cours, Matiere, Fichier
-
-
-# Register your models here.
-admin.site.register(Classe)
-admin.site.register(Cours)
-admin.site.register(Matiere)
-admin.site.register(Fichier)
